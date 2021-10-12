3c1e1fa1bd9e99bf85bca4d2fcaaba2be6c537f7
fwara07 <fanuelwara05@gmail.com>
2021-10-11 20:16:58 -0400

    Updates

--- precache-manifest.59d451394529ff781d890c15bd724994.js
+++ precache-manifest.59d451394529ff781d890c15bd724994.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "feb4c1e26022f523fe359477751a5c9d",
+    "revision": "c6405f42b8b7a324eb0a63f135aa1b9e",
     "url": "/flowchart/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flowchart/static/css/2.7722d2a3.chunk.css"
   },
   {
-    "revision": "85015108c8461b741685",
+    "revision": "4e70844e82dc0ccab424",
     "url": "/flowchart/static/css/main.21357b06.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/flowchart/static/js/2.e4659a66.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "85015108c8461b741685",
-    "url": "/flowchart/static/js/main.f6a6c87f.chunk.js"
+    "revision": "4e70844e82dc0ccab424",
+    "url": "/flowchart/static/js/main.ef2781c4.chunk.js"
   },
   {
     "revision": "fd74a3738492b7211dd5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flowchart/precache-manifest.4a00dc79fdcd20d456114b66dbc06b79.js"
+  "/flowchart/precache-manifest.59d451394529ff781d890c15bd724994.js"
 );
 
 self.addEventListener('message', (event) => {
