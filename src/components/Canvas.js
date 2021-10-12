28487153d9760fa841fb9085350cb00cd57fecb2
fwara07 <fanuelwara05@gmail.com>
2021-10-11 20:49:15 -0400

    added new features

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1220,7 +1220,7 @@ const Canvas = ({
               </>
             )}
           </Grid>
-          <Grid item xs={3}>
+          <Grid item xs={6}>
             <Typography variant="h6" style={{ fontWeight: "bold" }}>
               {`Project: ${currentFile.parentId}`}
             </Typography>
