f5d6a836b435d333206e96565d1e0a2f52c8a80e
fwara07 <fanuelwara05@gmail.com>
2021-10-14 20:21:50 -0400

    fix bugs

--- src/components/CustomEdge.js
+++ src/components/CustomEdge.js
@@ -39,25 +39,6 @@ export default function CustomEdge({
         <defs>
           <marker
             className="react-flow__arrowhead"
-            id={`marker-start-${id}`}
-            markerWidth="25"
-            markerHeight="30"
-            viewBox="-10 -10 20 20"
-            orient="auto"
-            refX={0}
-            refY="0"
-          >
-            <polyline
-              stroke="#BBBBC0"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="1"
-              fill="#BBBBC0"
-              points="-5,-4 0,0 -5,4 -5,-4"
-            />
-          </marker>
-          <marker
-            className="react-flow__arrowhead"
             id={`marker-${id}`}
             markerWidth="25"
             markerHeight="30"
@@ -83,7 +64,7 @@ export default function CustomEdge({
         className="react-flow__edge-path"
         style={style}
         markerEnd={`url(#marker-${id})`}
-        markerStart={`url(#marker-start-${id})`}
+        markerStart={`url(#marker-${id})`}
       />
     </>
   );
