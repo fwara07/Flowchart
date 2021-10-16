c8110f459478617153dc03cf4231708a1ce73f03
fwara07 <fanuelwara05@gmail.com>
2021-10-16 18:15:19 -0400

    Updates

---
+++ precache-manifest.b11846dd9077838e7e8dd113957563ae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb185209d535cdff01cde9b7b86f926",
+    "url": "/flowchart/index.html"
+  },
+  {
+    "revision": "213951ead527942d6c1c",
+    "url": "/flowchart/static/css/2.7722d2a3.chunk.css"
+  },
+  {
+    "revision": "549c4c062c6c4dab3ac2",
+    "url": "/flowchart/static/css/main.21357b06.chunk.css"
+  },
+  {
+    "revision": "213951ead527942d6c1c",
+    "url": "/flowchart/static/js/2.7c72693a.chunk.js"
+  },
+  {
+    "revision": "5e303b1dc5141fae2ad1b914c463becd",
+    "url": "/flowchart/static/js/2.7c72693a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549c4c062c6c4dab3ac2",
+    "url": "/flowchart/static/js/main.07b0e173.chunk.js"
+  },
+  {
+    "revision": "fd74a3738492b7211dd5",
+    "url": "/flowchart/static/js/runtime-main.af54d932.js"
+  },
+  {
+    "revision": "4519aea339b63bce6e1868865586f405",
+    "url": "/flowchart/static/media/dxicons.4519aea3.woff2"
+  },
+  {
+    "revision": "f1829bb1cb996a760a819b4eb23b6108",
+    "url": "/flowchart/static/media/dxicons.f1829bb1.ttf"
+  },
+  {
+    "revision": "fd4164a956b105e4ec9f6c12de2d6b82",
+    "url": "/flowchart/static/media/dxicons.fd4164a9.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flowchart/precache-manifest.45c3c919da9be50b3cebba58ab57d9b6.js"
+  "/flowchart/precache-manifest.b11846dd9077838e7e8dd113957563ae.js"
 );
 
 self.addEventListener('message', (event) => {
